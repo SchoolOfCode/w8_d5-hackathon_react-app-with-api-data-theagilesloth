b568f900ef983a034934b2db817b574ac3ff4e11
ryanglennsmith <ryan.glenn.smith@gmail.com>
2022-01-14 16:00:22 +0000

    fixed some typos and added error pg

--- src/layouts/home/home.component.jsx
+++ src/layouts/home/home.component.jsx
@@ -5,34 +5,34 @@ import { Buttons } from "../../components/buttons/buttons.component";
 import { Routes, Route, Link } from "react-router-dom";
 
 export function HomeLayout({ className }) {
-   const [searchValue, setSearchValue] = useState("");
+  const [searchValue, setSearchValue] = useState("");
 
-   return (
-      <div className={className} id="home-section">
-         <img id="home-image" src="/mountains-55067.png" alt="home" />
-         <div id="overlay" />
-         <div id="input-wrapper">
-            <h1>Search for Your Next Image</h1>
-            <Inputs
-               className="input"
-               onChange={(evt) => setSearchValue(evt.target.value)}
-               placeholder="Search"
-            />
+  return (
+    <div className={className} id="home-section">
+      <img id="home-image" src="/mountains-55067.png" alt="home" />
+      <div id="overlay" />
+      <div id="input-wrapper">
+        <h1>Search for Your Next Image</h1>
+        <Inputs
+          className="input"
+          onChange={(evt) => setSearchValue(evt.target.value)}
+          placeholder="Search"
+        />
 
-            <div id="btn-wrapper">
-               <Buttons className="search-btn">
-                  <Link className="link" to={`/results/${searchValue}`}>
-                     search
-                  </Link>
-               </Buttons>
+        <div id="btn-wrapper">
+          <Buttons className="search-btn">
+            <Link className="link" to={`/results/${searchValue}`}>
+              Search
+            </Link>
+          </Buttons>
 
-               <Buttons className="search-btn ">
-                  <Link className="link" to={`/`} >
-                     Im Feeling lucky
-                  </Link>
-               </Buttons>
-            </div>
-         </div>
+          <Buttons className="search-btn ">
+            <Link className="link" to={`/`}>
+              I'm Feeling Lucky!
+            </Link>
+          </Buttons>
+        </div>
       </div>
-   );
+    </div>
+  );
 }
